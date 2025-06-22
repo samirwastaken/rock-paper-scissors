e2b4001ae05fab35ab5b5c5a084c724dd1cc767b
Samir Katakamsetty <katakamsettysamir@gmail.com>
2025-06-22 01:01:15 -0400

    Added a way to get human input for the game

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const choices = ["Rock", "Paper", "Scissors"];
 
 
-// Random Number 
+// a better way of getting the random number
 function getRandomNumber(max)
 {
     return Math.floor(Math.random() * max);
-}
+};
 
 
 // Computer's Choice
@@ -18,5 +18,17 @@ function getComputerChoice()
 
     return computerChoice;
 
+};
+
+
+function getHumanChoice()
+{
+    let Choice = prompt("What will you choose? (0 = Rock, 1 = Paper, 2 = Scissors)");
+    let humanChoice = choices[parseInt(Choice)];
+
+    return humanChoice;
 }
 
+console.log(getComputerChoice());
+let humanChoice = getHumanChoice();
+console.log(humanChoice);
